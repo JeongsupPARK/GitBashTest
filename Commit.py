b68d7752675f8e770a6dfe20d133893bf9456c8f
Jeongsup Park <jspark3731@snu.ac.kr>
2022-11-21 23:46:24 +0900

    second_commit

--- Commit.py
+++ Commit.py
@@ -2,3 +2,6 @@ def when_did_you_commit():
 
     # This is written by Leader
     first_commit = ("2022-11-21 23:44PM")
+
+    # This is written by Leader
+    second_commit = ("2022-11-21 23:46PM")
